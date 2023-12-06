122417fb88e4662273091610d959d7794fdd9311
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-06 14:31:12 -0300

    feat04: implementa o método getClosestMuseum da camada de serviço

---
+++ src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
@@ -0,0 +1,8 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * The type Museum not found exception.
+ */
+public class MuseumNotFoundException extends RuntimeException{
+
+}
--- src/main/java/com/betrybe/museumfinder/service/MuseumService.java
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -2,9 +2,11 @@ package com.betrybe.museumfinder.service;
 
 import com.betrybe.museumfinder.database.MuseumFakeDatabase;
 import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
 import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.util.CoordinateUtil;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +24,16 @@ public class MuseumService implements MuseumServiceInterface {
 
   @Override
   public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
-    return null;
+    Boolean isCoordinateValid = CoordinateUtil.isCoordinateValid(coordinate);
+    if (!isCoordinateValid) {
+      throw new InvalidCoordinateException();
+    }
+
+    Optional<Museum> museum = database.getClosestMuseum(coordinate, maxDistance);
+    if (museum.isEmpty()) {
+      throw new MuseumNotFoundException();
+    }
+    return museum.get();
   }
 
   @Override
